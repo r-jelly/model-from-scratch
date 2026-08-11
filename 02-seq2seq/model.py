8fadd7cff3daac39c3ced38c83865efc1bc36da6
r-jelly <skydnk4332@gmail.com>
2026-08-11 23:27:22 +0900

    feat: add full seq-to-seq teacher-forced model

--- 02-seq2seq/model.py
+++ 02-seq2seq/model.py
@@ -18,8 +18,8 @@ class Seq2SeqEncoder(nn.Module):
     def forward(self, source: Tensor, valid_mask: Tensor):
         """
         Args:
-            source (LongTensor): (batch_size, seq_len)
-            valid_mask (BoolTensor): (batch_size, seq_len)
+            source (LongTensor): (batch_size, seq_len) 크기의 원본 시퀀스
+            valid_mask (BoolTensor): (batch_size, seq_len), 실제 source가 위치한 인덱스를 표시
         Returns:
             outputs (FloatTensor): 모든 timestep의 Encoder hidden state (batch_size, seq_len, hidden_size)
             hidden (FloatTensor): 각 sequence의 마지막 유효 hidden state (batch_size, hidden_size)
@@ -66,9 +66,46 @@ class Seq2SeqDecoder(nn.Module):
         return logit, (h_next, c_next)
 
 
+class Seq2Seq(nn.Module):
+    def __init__(self, vocab_size: int, embedding_size: int, hidden_size: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.encoder = Seq2SeqEncoder(
+            vocab_size=vocab_size,
+            embedding_size=embedding_size,
+            hidden_size=hidden_size
+        )
+        self.decoder = Seq2SeqDecoder(
+            vocab_size=vocab_size,
+            embedding_size=embedding_size,
+            hidden_size=hidden_size
+        )
+
+    def forward(self, source: Tensor, target_input: Tensor, valid_mask: Tensor):
+        """
+        Args:
+            source (LongTensor): (batch_size, seq_len) 크기의 원본 시퀀스
+            target_input (LongTensor): (batch_size, seq_len+1) 크기의 [BOS 토큰 + 정답 시퀀스]
+            valid_mask (BoolTensor): (batch_size, seq_len), 실제 source가 위치한 인덱스를 표시
+        Returns:
+            logits (FloatTensor): (batch_size, seq_len+1, vocab_size) 크기의 전체 logit
+        """
+        _, T = target_input.shape
+        _, (h_x, c_x) = self.encoder(source=source, valid_mask=valid_mask)
+
+        logits = []
+        for t in range(T):
+            logit, (h_x, c_x) = self.decoder(token=target_input[:,t], hidden=h_x, cell=c_x)
+            logits.append(logit)
+        logits = torch.stack(logits, dim=1)
+
+        return logits
+
 if __name__ == "__main__":
     random.seed(42)
     torch.manual_seed(42)
+
+    print("=" * 50)
+    print("1. Seq2Seq Encoder & Decoder Test")
 
     batch_size, min_seq, max_seq = 4, 3, 8
     source, target_input, target_output, valid_mask = \
@@ -100,3 +137,22 @@ if __name__ == "__main__":
     for name, parameter in decoder.named_parameters():
         assert parameter.grad is not None
         assert torch.isfinite(parameter.grad).all()
+
+    print("=" * 50)
+    print("2. Seq2Seq Model Test")
+    seq_to_seq = Seq2Seq(vocab_size=vocab_size, embedding_size=emb_size, hidden_size=hidden_size)
+    logits = seq_to_seq.forward(source=source, target_input=target_input, valid_mask=valid_mask)
+
+    assert torch.isfinite(logits).all()
+    assert logits.shape == (batch_size, target_input.shape[1], vocab_size)
+
+    logits = logits.reshape(-1, vocab_size)
+    target_output_flatten = target_output.reshape(-1)
+    loss = loss_fn(logits, target_output_flatten)
+    assert torch.isfinite(loss).all()
+
+    loss.backward()
+    for name, parameter in seq_to_seq.named_parameters():
+        assert parameter.grad is not None
+        assert torch.isfinite(parameter.grad).all()
+    
